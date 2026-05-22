db787b41d2dcc31d824eea52abb05646e34a1d9a
alexibba <ibbaa@hr.nl>
2026-05-23 01:44:39 +0200

    Lesson 14 improvements

--- Lessons/LessonWeek14/Code/Program.cs
+++ Lessons/LessonWeek14/Code/Program.cs
@@ -8,7 +8,7 @@ Stopwatch stopwatch_ = new Stopwatch();
 //Fibonacci recursive (non memoized => O(2^n))
 Func<long, long> fib = null;
 fib = n => n <= 1 ? n : fib(n - 1) + fib(n - 2);
-for(int num = 30; num <= 46; num++){
+for(int num = 30; num <= 45; num++){
     stopwatch_.Reset();
     stopwatch_.Start();
     var result = fib(num);
@@ -16,13 +16,13 @@ for(int num = 30; num <= 46; num++){
     System.Console.WriteLine($"Fib({num}) = {result}, Time elapsed: {stopwatch_.Elapsed}\n");
 }
 System.Console.WriteLine("\n");
-long n = rand.Next(50, 65);
+long n = rand.Next(60, 65);
 
 //>>>>> DO NOT uncomment the following line as this would take a lot of time...<<<<<
 //var fibNumber = fib(n);
 
 long[] intermediateResultsActual = new long[n + 1];
-// initialize the first two values in the array which we use as a map
+// initialize the first two values in the array which we use as a memo
 intermediateResultsActual[0] = 0;
 intermediateResultsActual[1] = 1;
 
@@ -32,24 +32,28 @@ stopwatch_.Start();
 var actual = DynamicProgramming.FibonacciDynamic(n, intermediateResultsActual);
 stopwatch_.Stop();
 
-System.Console.WriteLine($"\nFibMemoized({n}) = {actual}, Time elapsed: {stopwatch_.Elapsed}, recursive calls: {Utils.counter})");
+System.Console.WriteLine($"\n Estimation of time necessary to compute (without memoization) Fib({n}) = {actual} => last elapsed time of previous series multiplied by 2^{n - 45} = {Math.Pow(2, n - 45)}");
+System.Console.WriteLine($"\nFibMemoized({n}) = {actual}, Time elapsed: {stopwatch_.Elapsed}, recursive calls: {Utils.counter}");
 System.Console.WriteLine("\nIntermediate results:");
 var totalValues = intermediateResultsActual.ToList().Count(x => x != 0) + 1;
-intermediateResultsActual.Take(totalValues).ToList().ForEach(System.Console.WriteLine);
+long i = 0;
+intermediateResultsActual.Take(totalValues).Select(_ => $"Fib({i++}) = {_}").ToList().ForEach(System.Console.WriteLine);
 
 Utils.SetToZero();
 stopwatch_.Reset();
-n = rand.Next(50, 65); 
+n = rand.Next(48, 55); 
 intermediateResultsActual = new long[n + 1];
-// initialize the first two values in the array which we use as a map
+// initialize the first two values in the array which we use as a memo
 intermediateResultsActual[0] = 0;
 intermediateResultsActual[1] = 1;
 stopwatch_.Start();
-var res = DynamicProgramming.FibonacciDynamic(n, intermediateResultsActual);
+var res = DynamicProgramming.FibonacciDynamic_Wrong(n/2, intermediateResultsActual);
 stopwatch_.Stop();
-System.Console.WriteLine($"\n\nFibMemoized({n}) = {res}, Time elapsed: {stopwatch_.Elapsed}, recursive calls: {Utils.counter}\n");
+System.Console.WriteLine("\nNotice the number of recursive calls:");
+System.Console.WriteLine($"\n\n>>>> Fib_NOT_Memoized({n/2}) = {res}, Time elapsed: {stopwatch_.Elapsed}, recursive calls: {Utils.counter}\n");
 System.Console.WriteLine();
 
 //totalValues = intermediateResultsActual.ToList().Count(x => x != 0) + 1;
-//intermediateResultsActual.Take(totalValues).ToList().ForEach(System.Console.WriteLine);
+//long i = 0;
+//intermediateResultsActual.Take(totalValues).Select(_ => $"Fib({i++}) = {_}").ToList().ForEach(System.Console.WriteLine);
 System.Console.WriteLine();
--- Lessons/LessonWeek14/Code/Solution/FibonacciDynamic.cs
+++ Lessons/LessonWeek14/Code/Solution/FibonacciDynamic.cs
@@ -2,16 +2,51 @@ namespace Solution;
 
 public static class DynamicProgramming {
 
-    public static long FibonacciDynamic(long n, long[] storedResults)
+    public static long FibonacciDynamic(long n, long[] storedResults = null)
     {
         Utils.ShowCallStack(false); //DO NOT comment this line of code
+        
+        //initialization of memo:
+        if(storedResults == null)
+        {
+            storedResults = new long[n + 1];
+            storedResults[0] = 0;
+            storedResults[1] = 1;
+        }
 
-        if (n == 0) return 0;
+        if (n <= 1) return n;
         if (storedResults[n] == 0)
         {
             var nMinOne = FibonacciDynamic(n - 1, storedResults);
             var nMinTwo = FibonacciDynamic(n - 2, storedResults);
             storedResults[n] = nMinOne + nMinTwo;
+            //equivalent using one line:
+            //storedResults[n] = FibonacciDynamic(n - 1, storedResults) + FibonacciDynamic(n - 2, storedResults);
+        }
+        return storedResults[n];
+    }
+
+    //Not properly implemented, no actual memoization:
+    public static long FibonacciDynamic_Wrong(long n, long[] storedResults = null)
+    {
+        Utils.ShowCallStack(false); //DO NOT comment this line of code
+        
+        //initialization of memo:
+        if(storedResults == null)
+        {
+            storedResults = new long[n + 1];
+            storedResults[0] = 0;
+            storedResults[1] = 1;
+        }
+        
+        if (n <= 1) return n;
+        //if (storedResults[n] == 0) //Missing part to achieve memoization 
+        {
+            var nMinOne = FibonacciDynamic_Wrong(n - 1, storedResults);
+            var nMinTwo = FibonacciDynamic_Wrong(n - 2, storedResults);
+            storedResults[n] = nMinOne + nMinTwo;
+            //equivalent using one line:
+            //storedResults[n] = FibonacciDynamic_Wrong(n - 1, storedResults) + FibonacciDynamic_Wrong(n - 2, storedResults);
         }
         return storedResults[n];
     }
