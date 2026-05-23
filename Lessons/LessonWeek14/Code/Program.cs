8c5cc4882ed1717f204f3fcc96cd88eb333620f6
alexibba <ibbaa@hr.nl>
2026-05-23 20:58:16 +0200

    Lesson 14 Bottom Up code added

--- Lessons/LessonWeek14/Code/Program.cs
+++ Lessons/LessonWeek14/Code/Program.cs
@@ -4,16 +4,22 @@ using System.Diagnostics;
 
 var rand = new Random();
 Stopwatch stopwatch_ = new Stopwatch();
-
-//Fibonacci recursive (non memoized => O(2^n))
+//Fibonacci recursive (non memoized => O(2^n)):
 Func<long, long> fib = null;
 fib = n => n <= 1 ? n : fib(n - 1) + fib(n - 2);
-for(int num = 30; num <= 45; num++){
+
+for(int num = 30; num <= 46; num++){
     stopwatch_.Reset();
     stopwatch_.Start();
-    var result = fib(num);
+    var result = Fib(num);//fib(num);
     stopwatch_.Stop();
-    System.Console.WriteLine($"Fib({num}) = {result}, Time elapsed: {stopwatch_.Elapsed}\n");
+    System.Console.WriteLine($"FibRecursive({num}) = {result}, Time elapsed: {stopwatch_.Elapsed}");
+    
+    stopwatch_.Reset();
+    stopwatch_.Start();
+    var result_ = FibIterative(num);
+    stopwatch_.Stop();
+    System.Console.WriteLine($"FibIterative({num}) = {result_}, Time elapsed: {stopwatch_.Elapsed}\n");
 }
 System.Console.WriteLine("\n");
 long n = rand.Next(60, 65);
@@ -32,7 +38,7 @@ stopwatch_.Start();
 var actual = DynamicProgramming.FibonacciDynamic(n, intermediateResultsActual);
 stopwatch_.Stop();
 
-System.Console.WriteLine($"\n Estimation of time necessary to compute (without memoization) Fib({n}) = {actual} => last elapsed time of previous series multiplied by 2^{n - 45} = {Math.Pow(2, n - 45)}");
+System.Console.WriteLine($"\nEstimation of time necessary to compute (without memoization) Fib({n}) = {actual} => last elapsed time of previous series multiplied by 2^{n - 45} = {Math.Pow(2, n - 45)}");
 System.Console.WriteLine($"\nFibMemoized({n}) = {actual}, Time elapsed: {stopwatch_.Elapsed}, recursive calls: {Utils.counter}");
 System.Console.WriteLine("\nIntermediate results:");
 var totalValues = intermediateResultsActual.ToList().Count(x => x != 0) + 1;
@@ -56,4 +62,38 @@ System.Console.WriteLine();
 //totalValues = intermediateResultsActual.ToList().Count(x => x != 0) + 1;
 //long i = 0;
 //intermediateResultsActual.Take(totalValues).Select(_ => $"Fib({i++}) = {_}").ToList().ForEach(System.Console.WriteLine);
+
+System.Console.WriteLine("\nFibonacci Dynamic Tabulation/BottomUp:");
+int n_ = 0;
+stopwatch_.Reset();
+stopwatch_.Start();
+ var resBottomUp = DynamicProgramming.FibonacciBottomUp(50);
+stopwatch_.Stop();
+System.Console.WriteLine($"\nFibonacci Dynamic Tabulation/BottomUp (Time elapsed: {stopwatch_.Elapsed})");
+resBottomUp.Select(_ => $"Fib({n_++}) = {_}").ToList().ForEach(System.Console.WriteLine);
 System.Console.WriteLine();
+
+
+//Classical Fibonacci recursive (non memoized => O(2^n))
+static long Fib(long n)
+{  
+    if (n <= 1) return n;
+    return Fib(n - 1) + Fib(n - 2);
+}
+
+static long FibIterative(long n)
+{
+    if (n <= 1) return n;
+    
+    long previouFib = 0;
+    long currentFib = 1;
+
+    for(long i = 2; i <= n; ++i)
+    {
+       long newFib = previouFib + currentFib;
+       previouFib = currentFib;
+       currentFib = newFib;
+    }
+
+    return currentFib;
+}
--- Lessons/LessonWeek14/Code/Solution/FibonacciDynamic.cs
+++ Lessons/LessonWeek14/Code/Solution/FibonacciDynamic.cs
@@ -50,4 +50,20 @@ public static class DynamicProgramming {
         }
         return storedResults[n];
     }
+
+    public static long[] FibonacciBottomUp(long n, long[] memo = null){
+        //initialization of memo:
+        if(memo == null)
+        {
+            memo = new long[n + 1];
+            memo[0] = 0;
+            memo[1] = 1;
+        }
+
+        for(long i = 2; i < memo.Length; ++i){
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo;
+    }
+
 }    
